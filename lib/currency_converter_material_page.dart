46296c5dd0ddbe53e52e5c56d441adaa5ce23213
Dev-scott <sadoscott25@gmail.com>
2024-11-04 22:09:09 +0100

    work on border

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -6,32 +6,36 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
         body: Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text(
+          const Text(
             "0",
             style: TextStyle(fontSize: 45, fontWeight: FontWeight.bold),
           ),
-          TextField(
-            style: TextStyle(color: Colors.blue),
-            decoration: InputDecoration(
-              hintText: "Please enter the amount in usd",
-              hintStyle: TextStyle(color: Colors.white),
-              prefixIcon: Icon(Icons.monetization_on),
-              prefixStyle: TextStyle(color: Colors.white),
-              filled: true,
-              fillColor: Colors.black,
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                  color: Colors.red,
-                  width: 2.0,
-                  style: BorderStyle.solid
-                ),
-                borderRadius: BorderRadius.all(Radius.circular(80))
+          Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: const TextField(
+              style: TextStyle(color: Colors.blue),
+              decoration: InputDecoration(
+                  hintText: "Please enter the amount in usd",
+                  hintStyle: TextStyle(color: Colors.white),
+                  prefixIcon: Icon(Icons.monetization_on),
+                  prefixStyle: TextStyle(color: Colors.white),
+                  filled: true,
+                  fillColor: Colors.black,
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                          color: Colors.red,
+                          width: 2.0,
+                          style: BorderStyle.solid),
+                      borderRadius: BorderRadius.all(Radius.circular(80))),
+                  enabledBorder: UnderlineInputBorder()),
+              keyboardType: const TextInputType.numberWithOptions(
+                decimal: true,
               ),
             ),
           ),
